01947bcb08cf5a1cefff38d10eea21a5f2dea486
anamitrra <09charlie@protonmail.com>
2024-09-28 11:51:39 +0530

    category routes added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,6 +11,16 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::resource('category', CategoryController::class)->middleware(['auth', 'verified']);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/category', [CategoryController::class, 'index'])->name('category.get');
+    Route::post('/category', [CategoryController::class, 'store'])->name('category.create');
+    Route::patch('/category', [CategoryController::class, 'update'])->name('category.update');
+    Route::delete('/category', [CategoryController::class, 'destroy'])->name('category.destroy');
+});
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
