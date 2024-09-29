8465ab73971060349ef15e8d104312959440eee8
anamitrra <09charlie@protonmail.com>
2024-09-29 19:49:52 +0530

    php echo added to css and js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,8 +20,10 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <link rel="stylesheet" href="{{ $css }}" id="css">
-<script src="{{ $js }}" id="js"></script>
+    <link rel="stylesheet" href="<?php echo $css; ?>" id="css">
+    <script src='<?php echo $js; ?>'  id="js"></script>
+
+    
     <!-- Scripts -->
      <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
 </head>
