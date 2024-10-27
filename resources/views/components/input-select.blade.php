c1fe94fe27008ebf92c0e24d0a62c9606d98c35c
anamitrra <09charlie@protonmail.com>
2024-10-27 13:07:50 +0530

    blade components updated

--- resources/views/components/input-select.blade.php
+++ resources/views/components/input-select.blade.php
@@ -1,5 +1,5 @@
 @props(['disabled' => false, 'value'])
 
-<select {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm px-3 py-2']) !!}>
+<select {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border border-gray-300   focus:border-indigo-500  focus:ring-indigo-500  rounded-md shadow-sm px-3 py-2']) !!}>
     {{ $value ?? $slot }}
 </select>
